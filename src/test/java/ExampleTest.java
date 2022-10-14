41667264e65f4e2d5bf0bc42001d665c7c65193c
CrisMouta <crismouta@gmail.com>
2022-10-14 14:47:35 +0200

    test failing

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ExampleTest {
     @Test
-    public void test() {}
+    public void test() {
+        Assertions.assertEquals(1, 2);
+    }
 }
